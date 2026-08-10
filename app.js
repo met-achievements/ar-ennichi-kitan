ab05333b127b48d4fb609d467dbf2a9d39addb2d
shinshin <shinyailham@gmail.com>
2026-08-10 19:13:30 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const STORAGE_KEY = "arEnnichiKitanProgressV13";
+const STORAGE_KEY = "arEnnichiKitanProgressV14";
 
 const questions = [
   {
@@ -9,7 +9,7 @@ const questions = [
     letter: "ず",
     image: "./assets/images/d-center.jpg",
     imageAlt: "D-Centerの看板",
-    arPage: "./d-center-ar.html?v=13",
+    arPage: "./d-center-ar.html?v=14",
     arFlag: "d-center-ar-success",
     launchLabel: "D館ARを起動する",
     help: "D-Centerをなるべく画面に残したまま、階段とは反対側へゆっくり回り込んでください。"
